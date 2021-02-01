e66b5adc3aee271a40baec77b171d476c023f9e6
Shuning Bian <freespace@gmail.com>
2021-02-01 18:12:28 +1100

    Applies autoformatters to codebase

--- ebs.py
+++ ebs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from datetime import datetime, timedelta
+
 import boto3
 
 MAX_AVAILABLE = 20
@@ -9,6 +10,7 @@ MAX_EBS_SIZE = 10 * 1000
 
 REPORTING_LOOKBACK_TIME_HOURS = 48
 REPORTING_PERIOD_SECS = 5 * 60
+
 
 class Volume(dict):
   def __init__(self, volume_info):
@@ -27,15 +29,17 @@ class Volume(dict):
   @property
   def available_metrics(self):
     """
-    Poorly implemented only used for quick debugging
+    Poorly implemented only used for quick debugging.
     """
     cw = boto3.client('cloudwatch')
     metrics_vec = []
     nexttoken = ''
     done = False
     while not done:
-      resp = cw.list_metrics(Namespace='AWS/EBS',
-                             Dimensions=[{'Name': 'VolumeId', 'Value':self.VolumeId}])
+      resp = cw.list_metrics(
+          Namespace='AWS/EBS', Dimensions=[{
+              'Name': 'VolumeId', 'Value': self.VolumeId
+          }])
       for m in resp['Metrics']:
         metrics_vec.append(f'{m["Namespace"]}:{m["MetricName"]} D={m["Dimensions"]}')
 
@@ -52,15 +56,21 @@ class Volume(dict):
 
     cw = boto3.client('cloudwatch')
     now = datetime.utcnow()
-    resp = cw.get_metric_statistics(Namespace='AWS/EBS',
-                                    MetricName='VolumeReadOps',
-                                    Dimensions=[{'Name': 'VolumeId', 'Value':self.VolumeId}],
-                                    StartTime=now - timedelta(hours=REPORTING_LOOKBACK_TIME_HOURS),
-                                    EndTime=now.utcnow(),
-                                    Period=REPORTING_PERIOD_SECS,
-                                    Statistics=['Sum'],
-                                    Unit='None')
+    resp = cw.get_metric_statistics(
+        Namespace='AWS/EBS',
+        MetricName='VolumeReadOps',
+        Dimensions=[{
+            'Name': 'VolumeId', 'Value': self.VolumeId
+        }],
+        StartTime=now - timedelta(hours=REPORTING_LOOKBACK_TIME_HOURS),
+        EndTime=now.utcnow(),
+        Period=REPORTING_PERIOD_SECS,
+        Statistics=['Sum'],
+        Unit='None')  # noqc
+
+    assert resp
     return -1
+
 
 def ebs_reaper(slack_send_func=None):
   """
@@ -82,10 +92,7 @@ def ebs_reaper(slack_send_func=None):
   done = False
   nexttoken = ''
   while not done:
-    resp = ec2.describe_volumes(Filters=[{
-        'Name': 'status',
-        'Values': ['in-use', 'available']
-    }])
+    resp = ec2.describe_volumes(Filters=[{'Name': 'status', 'Values': ['in-use', 'available']}])
 
     nexttoken = resp.get('NextToken')
     if nexttoken is None:
@@ -96,12 +103,12 @@ def ebs_reaper(slack_send_func=None):
 
   total_size = sum(v.Size for v in volumes_vec)
   available_volumes = [v for v in volumes_vec if v.State == 'available']
-  inuse_volumes_over_200GB = [v for v in volumes_vec if v.State == 'in-use' and v.Size >200]
+  inuse_volumes_over_200GB = [v for v in volumes_vec if v.State == 'in-use' and v.Size > 200]
 
   for v in volumes_vec:
     print(f'{v.name:64s}  {v.State:10s}  {v.Size:6d} GB')
 
-  print('-'*87)
+  print('-' * 87)
   print(f'{"Total Size":64s}  {"":10s}  {total_size:6d} GB')
 
   def warn(msg):
@@ -115,14 +122,16 @@ def ebs_reaper(slack_send_func=None):
 
   n_inuse_over_200GB = len(inuse_volumes_over_200GB)
   if n_inuse_over_200GB > MAX_IN_USE_OVER_200GB:
-    msg = (f'⚠️ Number of in-use volumes > 200GB ({n_inuse_over_200GB}) exceeds '
-           f'threshold ({MAX_IN_USE_OVER_200GB}) ⚠️')
+    msg = (
+        f'⚠️ Number of in-use volumes > 200GB ({n_inuse_over_200GB}) exceeds '
+        f'threshold ({MAX_IN_USE_OVER_200GB}) ⚠️')
     warn(msg)
 
   if total_size > MAX_EBS_SIZE:
     warn(f'⚠️ Total EBS size ({total_size} GB) exceeds threshold ({MAX_EBS_SIZE} GB) ⚠️')
 
   # TODO: implement idleness checks here once Volume.time_spent_idle is implemented
+
 
 def test_smoke():
   ebs_reaper()
--- reaper.py
+++ reaper.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-import re
-import os
 import json
+import os
+import re
 from datetime import datetime, timedelta
 
-import click
 import boto3
+import click
 import requests
 
 from ebs import ebs_reaper
@@ -14,7 +14,7 @@ from ebs import ebs_reaper
 # the small unit of time over which we report on
 # statistics. i.e. when we ask for an average of a value
 # it is average over REPORTING_PERIOD_SECS
-REPORTING_PERIOD_SECS = 10*60
+REPORTING_PERIOD_SECS = 10 * 60
 
 # this the number of hours we look back when computing
 # utilisation
@@ -25,6 +25,7 @@ SLACK_CHANNEL_ENV_VAR = 'SLACK_CHANNEL'
 
 INSTANCE_TYPE_REGEX = re.compile('.large')
 
+
 def slack_send(msg):
   webhook = os.environ.get(SLACK_WEB_HOOK_ENV_VAR)
   channel = os.environ.get(SLACK_CHANNEL_ENV_VAR)
@@ -32,9 +33,7 @@ def slack_send(msg):
   if webhook is None:
     raise Exception(f'Please set environment variable {SLACK_WEB_HOOK_ENV_VAR}')
 
-  payload = dict(text=msg,
-                 username='ec2-reaper',
-                 icon_emoji=':robot_face:')
+  payload = dict(text=msg, username='ec2-reaper', icon_emoji=':robot_face:')
   if channel:
     payload['channel'] = channel
 
@@ -42,16 +41,22 @@ def slack_send(msg):
   if req.status_code != 200:
     print('Response:', req.status_code, req.content)
 
+
 def slack_warn(inst):
-  msg = f':warning: Instance {inst.name} ({inst.InstanceId}) has been idle for {inst.idle_period_hours:.2f} hours '
-  f'and will be stopped soon.'
+  msg = (
+      f':warning: Instance {inst.name} ({inst.InstanceId}) has been idle for {inst.idle_period_hours:.2f} hours '
+      'and will be stopped soon.')
   slack_send(msg)
 
+
 def stop_instance(inst):
-  msg = f':warning: Instance {inst.name} ({inst.InstanceId}) has been idle for {inst.idle_period_hours:.2f} hours '
-  f'and would be stopped if Steve implemented automatically termination. He has not.'
+  msg = (
+      f':warning: Instance {inst.name} ({inst.InstanceId}) has been idle for {inst.idle_period_hours:.2f} hours '
+      'and would be stopped if Steve implemented automatically termination. He has not.')
+
   slack_send(msg)
   print(f'Stopping instance {inst}')
+
 
 class Instance(dict):
   def __init__(self, instance_dict, min_cpu_utilisation, min_disk_ops, min_network_packets):
@@ -83,8 +88,10 @@ class Instance(dict):
 
   @property
   def network_idle_period_hours(self):
-    packets_in_idle = self.get_idle_period_hours_for_metric('NetworkPacketsIn', 'Count', self._min_network)
-    packets_out_idle = self.get_idle_period_hours_for_metric('NetworkPacketsOut', 'Count', self._min_network)
+    packets_in_idle = self.get_idle_period_hours_for_metric(
+        'NetworkPacketsIn', 'Count', self._min_network)
+    packets_out_idle = self.get_idle_period_hours_for_metric(
+        'NetworkPacketsOut', 'Count', self._min_network)
 
     return max(packets_in_idle, packets_out_idle)
 
@@ -98,27 +105,30 @@ class Instance(dict):
 
     disk_write_idle = self.get_idle_period_hours_for_metric('EBSWriteOps', 'Count', self._min_disk)
     if disk_write_idle < 0:
-      disk_write_idle = self.get_idle_period_hours_for_metric('DiskWriteOps', 'Count', self._min_disk)
+      disk_write_idle = self.get_idle_period_hours_for_metric(
+          'DiskWriteOps', 'Count', self._min_disk)
 
     return max(disk_read_idle, disk_write_idle)
 
   @property
   def idle_period_hours(self):
-    return min(self.cpu_idle_period_hours,
-               self.disk_idle_period_hours,
-               self.network_idle_period_hours)
+    return min(
+        self.cpu_idle_period_hours, self.disk_idle_period_hours, self.network_idle_period_hours)
+
   @property
   def available_metrics(self):
     """
-    Poorly implemented only used for quick debugging
+    Poorly implemented only used for quick debugging.
     """
     cw = boto3.client('cloudwatch')
     metrics_vec = []
     nexttoken = ''
     done = False
     while not done:
-      resp = cw.list_metrics(Namespace='AWS/EC2',
-                             Dimensions=[{'Name': 'InstanceId', 'Value':self.InstanceId}])
+      resp = cw.list_metrics(
+          Namespace='AWS/EC2', Dimensions=[{
+              'Name': 'InstanceId', 'Value': self.InstanceId
+          }])
       for m in resp['Metrics']:
         metrics_vec.append(f'{m["Namespace"]}:{m["MetricName"]}')
 
@@ -130,19 +140,24 @@ class Instance(dict):
 
   def get_idle_period_hours_for_metric(self, metricname, unit, idle_threshold):
     """
-    Returns the number of hours for which the specified metric was below the idle_threshold.
+    Returns the number of hours for which the specified metric was below the
+    idle_threshold.
 
-    If the metric doesn't exist or returns no data points then -1 is returned.
+    If the metric doesn't exist or returns no data points then -1 is
+    returned.
     """
     cw = boto3.client('cloudwatch')
-    resp = cw.get_metric_statistics(Namespace='AWS/EC2',
-                                    MetricName=metricname,
-                                    Dimensions=[{'Name': 'InstanceId', 'Value':self.InstanceId}],
-                                    StartTime=datetime.utcnow() - timedelta(hours=REPORTING_LOOKBACK_TIME_HOURS),
-                                    EndTime=datetime.utcnow(),
-                                    Period=REPORTING_PERIOD_SECS,
-                                    Statistics=['Average'],
-                                    Unit=unit)
+    resp = cw.get_metric_statistics(
+        Namespace='AWS/EC2',
+        MetricName=metricname,
+        Dimensions=[{
+            'Name': 'InstanceId', 'Value': self.InstanceId
+        }],
+        StartTime=datetime.utcnow() - timedelta(hours=REPORTING_LOOKBACK_TIME_HOURS),
+        EndTime=datetime.utcnow(),
+        Period=REPORTING_PERIOD_SECS,
+        Statistics=['Average'],
+        Unit=unit)
 
     idle_periods = []
     datapoints = resp['Datapoints']
@@ -156,7 +171,6 @@ class Instance(dict):
 
     if len(idle_periods):
       idle_periods.sort(reverse=True)
-
 
       # find when idle period started by going back in time and finding when was
       # the last time there was more than a reporting period between idleness
@@ -177,33 +191,60 @@ class Instance(dict):
   def __str__(self):
     return f'{self.name:64s}  {self.InstanceId}  {self.InstanceType:16s} {self.state}'
 
+
 @click.command()
-@click.option('-c', '--min-cpu-utilisation', type=float, default=3,
-              help='Minimum CPU utilisation in % per 5 minutes for an instance '
-                   'to be considered as active')
-@click.option('-d', '--min-disk-ops', type=float, default=1,
-              help='Minimum disk operations (read or write) per 5 minutes for an '
-                   'instance to be considered active')
-@click.option('-n', '--min-network-packets', type=int, default=100,
-              help='Minimum (in or out) network packets per 5 minutes for '
-                   'an instance to be considered active')
-@click.option('-s', '--stop_instance-idle-timeout-hours', type=int, default=6,
-              help='After this number of hours of being idle the instance will '
-                   'be stop_instance')
-@click.option('-w', '--warning-idle-timeout-hours', type=int, default=4,
-              help='After this number of hours of being idle a warning will be '
-                   'sent that the instance will be stop_instance soon-ish')
+@click.option(
+    '-c',
+    '--min-cpu-utilisation',
+    type=float,
+    default=3,
+    help='Minimum CPU utilisation in % per 5 minutes for an instance '
+    'to be considered as active')
+@click.option(
+    '-d',
+    '--min-disk-ops',
+    type=float,
+    default=1,
+    help='Minimum disk operations (read or write) per 5 minutes for an '
+    'instance to be considered active')
+@click.option(
+    '-n',
+    '--min-network-packets',
+    type=int,
+    default=100,
+    help='Minimum (in or out) network packets per 5 minutes for '
+    'an instance to be considered active')
+@click.option(
+    '-s',
+    '--stop_instance-idle-timeout-hours',
+    type=int,
+    default=6,
+    help='After this number of hours of being idle the instance will '
+    'be stop_instance')
+@click.option(
+    '-w',
+    '--warning-idle-timeout-hours',
+    type=int,
+    default=4,
+    help='After this number of hours of being idle a warning will be '
+    'sent that the instance will be stop_instance soon-ish')
 @click.option('-v', '--verbose', is_flag=True)
-@click.option('--include-stopped', is_flag=True,
-              help='Include stopped instances in idleness checks. For debugging '
-                   'mostly')
-@click.option('--dry-run', is_flag=True,
-              help='If given then no action will be take and no messages will be sent')
-@click.option('--test-slack', type=str, default=None,
-              help='When given a test message is sent into slack. Intended for '
-                   'verifying slack integration. No other action will be taken')
-@click.option('-r', '--region', type=str, default=None,
-              help='The AWS region to check')
+@click.option(
+    '--include-stopped',
+    is_flag=True,
+    help='Include stopped instances in idleness checks. For debugging '
+    'mostly')
+@click.option(
+    '--dry-run',
+    is_flag=True,
+    help='If given then no action will be take and no messages will be sent')
+@click.option(
+    '--test-slack',
+    type=str,
+    default=None,
+    help='When given a test message is sent into slack. Intended for '
+    'verifying slack integration. No other action will be taken')
+@click.option('-r', '--region', type=str, default=None, help='The AWS region to check')
 def main(*args, **kwargs):
   if kwargs['test_slack']:
     slack_send(kwargs['test_slack'])
@@ -223,25 +264,29 @@ def main(*args, **kwargs):
   print('==========')
   ebs_reaper(slack_send_func=slack_send)
 
-def reaper(min_cpu_utilisation,
-           min_disk_ops,
-           min_network_packets,
-           stop_instance_idle_timeout_hours,
-           warning_idle_timeout_hours,
-           verbose,
-           dry_run,
-           include_stopped,
-           warning_callback,
-           stop_instance_callback,
-           region):
+
+def reaper(
+    min_cpu_utilisation,
+    min_disk_ops,
+    min_network_packets,
+    stop_instance_idle_timeout_hours,
+    warning_idle_timeout_hours,
+    verbose,
+    dry_run,
+    include_stopped,
+    warning_callback,
+    stop_instance_callback,
+    region):
 
   if stop_instance_idle_timeout_hours > REPORTING_LOOKBACK_TIME_HOURS:
-    raise Exception(f'stop_instance idle timeout ({stop_instance_idle_timeout_hours}) cannot be longer than '
-                    f'{REPORTING_LOOKBACK_TIME_HOURS} hours.')
+    raise Exception(
+        f'stop_instance idle timeout ({stop_instance_idle_timeout_hours}) cannot be longer than '
+        f'{REPORTING_LOOKBACK_TIME_HOURS} hours.')
 
   if warning_idle_timeout_hours > REPORTING_LOOKBACK_TIME_HOURS:
-    raise Exception(f'warning idle timeout ({warning_idle_timeout_hours}) cannot be longer than '
-                    f'{REPORTING_LOOKBACK_TIME_HOURS} hours.')
+    raise Exception(
+        f'warning idle timeout ({warning_idle_timeout_hours}) cannot be longer than '
+        f'{REPORTING_LOOKBACK_TIME_HOURS} hours.')
   ec2 = boto3.client('ec2', region_name=region)
 
   instances_vec = []
@@ -262,7 +307,7 @@ def reaper(min_cpu_utilisation,
   if verbose:
     print('Instances')
     print('=========')
-    instances_vec.sort(key=lambda i:i.state)
+    instances_vec.sort(key=lambda i: i.state)
     for inst in instances_vec:
       print(inst)
 
@@ -299,15 +344,16 @@ def reaper(min_cpu_utilisation,
         stopped += 1
 
     if verbose:
-      print(f'{inst.name} cpu_idle={inst.cpu_idle_period_hours:.2f} '
-            f'net_idle={inst.network_idle_period_hours:.2f}',
-            f'disk_idle={inst.disk_idle_period_hours:.2f}')
+      print(
+          f'{inst.name} cpu_idle={inst.cpu_idle_period_hours:.2f} '
+          f'net_idle={inst.network_idle_period_hours:.2f}',
+          f'disk_idle={inst.disk_idle_period_hours:.2f}')
 
   print('')
   print('Summary')
   print('=======')
   print(f'Checked {checked} instances, issued {warned} warnings and stopped {stopped}.')
 
+
 if __name__ == '__main__':
   main()
-
